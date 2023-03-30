ce35e970b3466cf4dfae9c4aa8e858be51196eb6
theSamuelIyeh <105016275+theSamuelIyeh@users.noreply.github.com>
2023-03-30 10:34:18 +0100

    Complete /tags/list route

--- routes/tags.js
+++ routes/tags.js
@@ -1,9 +1,25 @@
 const express = require("express");
+const axios = require("axios").default;
 const router = express.Router();
 
 //get list of tags
-router.get('/list', (req, res) => {
-
+router.get("/list", async (req, res) => {
+  const params = req.query;
+  console.log(params);
+  try {
+    const response = await axios.get(
+      "https://yummly2.p.rapidapi.com/tags/list/",
+      {
+        headers: {
+          "X-RapidAPI-Key": process.env.RAPID_API_KEY,
+          "X-RapidAPI-Host": process.env.RAPID_API_HOST,
+        },
+      }
+    );
+    res.json(response.data);
+  } catch (error) {
+    res.json(error);
+  }
 });
 
 module.exports = router;
