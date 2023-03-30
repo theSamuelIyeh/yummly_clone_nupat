71f25c82f524cfa9feece9d2f8430f303217beec
theSamuelIyeh <105016275+theSamuelIyeh@users.noreply.github.com>
2023-03-30 12:58:58 +0100

    Complete /feeds route

--- routes/feeds.js
+++ routes/feeds.js
@@ -1,25 +1,97 @@
 const express = require("express");
-const axios = require('axios').default;
+const axios = require("axios").default;
 const router = express.Router();
 
 //get all ingredients and searches containing the parameter q
-router.get('/auto-complete', (req, res) => {
 
+router.get("/auto-complete", async (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  const params = req.query;
+  console.log(params);
+  try {
+    const response = await axios.get(
+      "https://yummly2.p.rapidapi.com/feeds/auto-complete/",
+      {
+        params: params,
+        headers: {
+          "X-RapidAPI-Key": process.env.RAPID_API_KEY,
+          "X-RapidAPI-Host": process.env.RAPID_API_HOST,
+        },
+      }
+    );
+    res.send(JSON.stringify(response.data, null, 3));
+  } catch (error) {
+    res.send(JSON.stringify(error, null, 3));
+  }
 });
 
 //get all items containing the search parameter
-router.get('/search', (req, res) => {
 
+router.get("/search", async (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  const params = req.query;
+  console.log(params);
+  try {
+    const response = await axios.get(
+      "https://yummly2.p.rapidapi.com/feeds/search/",
+      {
+        params: params,
+        headers: {
+          "X-RapidAPI-Key": process.env.RAPID_API_KEY,
+          "X-RapidAPI-Host": process.env.RAPID_API_HOST,
+        },
+      }
+    );
+    res.send(JSON.stringify(response.data, null, 3));
+  } catch (error) {
+    res.send(JSON.stringify(error, null, 3));
+  }
 });
 
 //get list
-router.get('/list', (req, res) => {
 
+router.get("/list", async (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  const params = req.query;
+  console.log(params);
+  try {
+    const response = await axios.get(
+      "https://yummly2.p.rapidapi.com/feeds/list/",
+      {
+        params: params,
+        headers: {
+          "X-RapidAPI-Key": process.env.RAPID_API_KEY,
+          "X-RapidAPI-Host": process.env.RAPID_API_HOST,
+        },
+      }
+    );
+    res.send(JSON.stringify(response.data, null, 3));
+  } catch (error) {
+    res.send(JSON.stringify(error, null, 3));
+  }
 });
 
 // get list similarities
-router.get('/list-similarities', (req, res) => {
 
+router.get("/list-similarities", async (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  const params = req.query;
+  console.log(params);
+  try {
+    const response = await axios.get(
+      "https://yummly2.p.rapidapi.com/feeds/list-similarities/",
+      {
+        params: params,
+        headers: {
+          "X-RapidAPI-Key": process.env.RAPID_API_KEY,
+          "X-RapidAPI-Host": process.env.RAPID_API_HOST,
+        },
+      }
+    );
+    res.send(JSON.stringify(response.data, null, 3));
+  } catch (error) {
+    res.send(JSON.stringify(error, null, 3));
+  }
 });
 
 module.exports = router;
