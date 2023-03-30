7f9202443da42887e1ef9448e1473b31315440b0
theSamuelIyeh <105016275+theSamuelIyeh@users.noreply.github.com>
2023-03-30 10:57:20 +0100

    Complete /categories/list route

--- routes/categories.js
+++ routes/categories.js
@@ -1,9 +1,27 @@
 const express = require("express");
+const axios = require('axios').default;
 const router = express.Router();
 
 //get list of categories
-router.get('/list', (req, res) => {
-
+router.get('/list', async (req, res) => {
+    res.setHeader("Content-Type", "application/json");
+  const params = req.query;
+  console.log(params);
+  try {
+    const response = await axios.get(
+      "https://yummly2.p.rapidapi.com/categories/list/",
+      {
+        params: params,
+        headers: {
+          "X-RapidAPI-Key": process.env.RAPID_API_KEY,
+          "X-RapidAPI-Host": process.env.RAPID_API_HOST,
+        },
+      }
+    );
+    res.send(JSON.stringify(response.data, null, 3));
+  } catch (error) {
+    res.send(JSON.stringify(error, NULL, 3));
+  }
 });
 
 module.exports = router;
