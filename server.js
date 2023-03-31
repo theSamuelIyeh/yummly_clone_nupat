fd2e197cbfd066c4d7304cc0d3ddeb5ed5975838
theSamuelIyeh <105016275+theSamuelIyeh@users.noreply.github.com>
2023-03-31 06:48:30 +0100

    Add index page

--- server.js
+++ server.js
@@ -10,12 +10,17 @@ const feedRouter = require("./routes/feeds");
 const categoryRouter = require("./routes/categories");
 const reviewRouter = require("./routes/reviews");
 const tagRouter = require("./routes/tags");
+const indexRouter = require("./routes/index");
 
 app.use(bodyParser.json());
 app.use("/feeds", feedRouter);
 app.use("/categories", categoryRouter);
 app.use("/reviews", reviewRouter);
 app.use("/tags", tagRouter);
+app.use("/", indexRouter);
+
+app.use(express.static(__dirname + '/public'));
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server started on port: ${port}`));
